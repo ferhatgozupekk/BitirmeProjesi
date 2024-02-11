e6acd4d185b897f63f44df2343e4c4ab464da721
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2024-02-11 23:25:02 +0300

    proje kurulumu yapıldı ve gerekli eklentiler yüklendi

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { GiHealthCapsule } from "react-icons/gi";
+
+function App() {
+  return (
+    <>
+      <GiHealthCapsule size={60} color="#4CAF50" />
+    </>
+  );
+}
+
+export default App;
